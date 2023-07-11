d058e6e8a4dc1ffc87e7363aea1f982f9a8562f7
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-11 23:21:46 +0900

    feat: Layout, Login 컴포넌트 생성

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+/** 레이아웃 컴포넌트 */
+const Layout = (props) => {
+    const { children } = props;
+    return <div>{children}</div>;
+};
+
+export default Layout;
---
+++ src/components/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/** 로그인 화면 컴포넌트 */
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
